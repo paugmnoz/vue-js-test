6d934b460f0cfbaeecffcc7cab692c86e88d80f7
Paula <paulaagarcia1@gmail.com>
2018-07-21 13:45:49 -0500

    Directives examples

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="test">
     <h1>{{title}}</h1>
+    <input type="text" v-model="title" name="" id="">
+    </br>
     <span v-html="title2"> </span>
-    <p>{{user.first_name}}</p>
+    <p v-if="showName">{{user.first_name}} showname is true</p>
+    <p v-else="showName">{{user.first_name}} showname is false</p>
     <p v-text='user.first_name'></p>
+    <ul>
+      <li v-for="item in items">{{item.title}}</li>
+    </ul>
+    <button v-on:click="greet('Hi')">Say hi</button>
+    <input type="text" name="" id="" v-on:keyup="pressKey" v-on:keyup.enter="enterHit">
   </div>
 </template>
 
@@ -20,8 +28,29 @@
                     first_name: 'Paula',
                     last_name: 'Garcia'
                 },
-                active: true
+                showName: false,
+                items: [{
+                    title: 'Item 1'
+                }, {
+                    title: 'Item 2'
+                }, {
+                    title: 'Item 3'
+                }]
             }
+
+        },
+
+        methods: {
+            greet: function(greeting) {
+                alert(greeting);
+            },
+            pressKey: function(e) {
+                console.log(e.target.value);
+            },
+            enterHit: function() {
+                console.log('You hit enter');
+            }
+
         }
     }
 </script>
